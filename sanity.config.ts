71cf3ede353e786466d378f9b9dc5868fbe1f12d
Mathias Riis Sørensen <72079659+mathiasrscom@users.noreply.github.com>
2025-03-01 07:56:45 +0100

    feat: adding vercel deploy

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,9 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
+// Plugins
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
+
 export default defineConfig({
   name: 'default',
   title: 'Drugrebels',
@@ -10,7 +13,7 @@ export default defineConfig({
   projectId: '9gpurr7n',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), vercelDeployTool()],
 
   schema: {
     types: schemaTypes,
